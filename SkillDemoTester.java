f32e8fdc4b5d8e574b9de0de0a9b6a91fb208eb5
KXVlNY <97643580+KXVlNY@users.noreply.github.com>
2022-02-04 01:40:01 -0800

    Tester + starter code

---
+++ SkillDemoTester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTester {
+    @Test
+    public void addition() {
+        assertEquals(2, SkillDemo.addition(1, 2)); // Failing at first :(
+    }
+}
