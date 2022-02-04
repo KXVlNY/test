ee94f752235cc6b2c47c6b4d9d32af1d4c1540fb
KXVlNY <97643580+KXVlNY@users.noreply.github.com>
2022-02-04 01:42:22 -0800

    Updated Tester

--- SkillDemoTester.java
+++ SkillDemoTester.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTester {
     @Test
     public void addition() {
-        assertEquals(2, SkillDemo.addition(1, 2)); // Failing at first :(
+        assertEquals(2, SkillDemo.addition(1, 1)); // Now it should be worked
     }
 }
